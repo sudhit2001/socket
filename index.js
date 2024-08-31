d2bb76ffae39c25097153cf6ef971fdebc4a836d
sudhit2001 <sudhit21soni@gmail.com>
2024-08-31 09:36:09 +0530

    Initial

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Socket.io
+io.on("connection", (socket) => {
+
+  socket.on("user-message", (message) => {
+    io.emit("message", message);
+    const intMessage = parseInt(message, 10);
+
+    if (!isNaN(intMessage)) {
+        const incrementedMessage = intMessage + 1;
+
+        io.emit("message", incrementedMessage);
+
+     }
+});
+});
+
+app.use(express.static(path.resolve("./public")));
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+server.listen(9000, () => console.log(`Server Started at PORT:9000`));
